cda446df362e63af5c0e9a6a1bcfa7e9f3cf7fcf
Olga Volkova <olyaafl@mail.ru>
2022-04-26 14:27:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 5000; // цена билета
+        int bonusSum = 20; // рубли для одной бонусной мили
+        int bonusMile = ticketPrice / bonusSum;
+        System.out.println("Вам начисленно бонусных миль:");
+        System.out.println(bonusMile + "миль");
+
+
+
+
+    }
+}
